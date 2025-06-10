e6fa8be1372aeb370b6bbb950bbe65f0a835e2d6
cdoge25 <hainguyenducthien@gmail.com>
2025-06-10 15:53:53 +0700

    fact timesheet

--- dbt_payroll/models/marts/fact/fact_timesheet.sql
+++ dbt_payroll/models/marts/fact/fact_timesheet.sql
@@ -13,20 +13,28 @@ employees AS (
     SELECT *
     FROM {{ ref('dim_employees') }}
 ),
+contracts AS (
+    SELECT *
+    FROM {{ ref('dim_contracts') }}
+),
 timesheet_joined AS (
     SELECT
         t.timesheet_id AS timesheet_pk,
         e.employee_pk AS employee_fk,
+        c.contract_pk AS contract_fk,
         t.timesheet_transaction_date,
         TO_NUMBER(TO_CHAR(t.timesheet_transaction_date, 'YYYYMMDD')) AS timesheet_transaction_date_fk,
         t.start_time,
         t.end_time,
-        t.timesheet_transaction_hours,
-        t.pay_period_id AS pay_period_fk
+        t.timesheet_transaction_hours
     FROM timesheet t
     LEFT JOIN employees e
         ON t.employee_id = e.employee_id
+    LEFT JOIN contracts c
+        ON t.employee_id = c.employee_id
+        AND t.timesheet_transaction_date BETWEEN c.start_date AND COALESCE(c.end_date, '9999-12-31')
     WHERE e.dbt_valid_to IS NULL
+    AND c.dbt_valid_to IS NULL
 )
 SELECT *
 FROM timesheet_joined
